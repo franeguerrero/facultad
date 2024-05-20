04d252cca0271174dc422a51ad2d4e5e333d296b
Juan Gomez <juanignaciogomez06@gmail.com>
2024-05-20 18:07:58 -0300

    punto numeros primos

---
+++ AyED/TP_AyEs_Punto8.cpp
@@ -0,0 +1,29 @@
+/*Desarrolle una funcion Primos, que genere en pantalla al listado de numeros primos ubicados
+entre 1 hasta un numero X (recibido como parametro).*/
+#include <iostream>
+using namespace std;
+int main(void){
+    int N; //Declaro variable
+
+    cout << "Ingrese un numero entero positivo: " <<endl;
+    cin >> N;
+
+    for(int k=2; k<N; k++){
+
+        int creciente = 2;
+        bool esPrimo = true;
+
+        while(esPrimo && creciente < k){
+
+            if(k % creciente == 0){ //verificando si la division es exacta
+             esPrimo = false;
+             }
+             else{
+             creciente++;
+             }
+        }
+
+        if(esPrimo)
+            cout << k << " es un numero primo." <<endl; 
+    }
+}
